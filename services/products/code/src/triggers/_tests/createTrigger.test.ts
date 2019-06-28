e04c97ff738db66b592e38b5db20e8f12a73a5fb
Gary Jennings <garysjennings@gmail.com>
2019-06-28 12:02:01 -0600

    added trigger test validate helper function

--- services/products/code/src/triggers/_tests/createTrigger.test.ts
+++ services/products/code/src/triggers/_tests/createTrigger.test.ts
@@ -1,14 +1,37 @@
 import { create } from '../index'
 
-describe('create', () => {
-    test('will throw error if input is not valid', async done => {
+const validInput = () => ({
+    name: 'Dark Coffee',
+    description: 'text...',
+    price: 400,
+    type: 'Coffee',
+    imrUrl: 'site.com/img.jpg'
+})
+
+const allKeysAreValidated = async x => {
+    let invalidMessages = 0
+    let keyLength = 0
+    const keys = Object.keys(x)
+    keyLength = keys.length
+
+    for (const key of keys) {
+        let invalid = validInput()
+        delete invalid[key]
         try {
-            await create({
-                name: 'invalid'
-            })
+            await create(invalid)
         } catch (e) {
             expect(e.message).toBe('Invalid Input')
-            done()
+            invalidMessages++
         }
+    }
+
+    return keyLength === invalidMessages
+}
+
+describe('create', () => {
+    test('will throw error if input is not valid', async done => {
+        const x = await allKeysAreValidated(validInput())
+        expect(x).toBe(true)
+        done()
     })
 })
